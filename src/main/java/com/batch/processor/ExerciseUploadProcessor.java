4f7ebc0ae540c6b800338ddfb4abd7331554fd31
huy <huyt71@gmail.com>
2021-02-28 22:20:21 +0700

    c

--- src/main/java/com/batch/processor/ExerciseUploadProcessor.java
+++ src/main/java/com/batch/processor/ExerciseUploadProcessor.java
@@ -193,9 +193,9 @@ public class ExerciseUploadProcessor implements ItemProcessor<ProcessorInput, Pr
 					QuestionBody questionBody = new QuestionBody();
 					questionBody.setNumber(String.valueOf(Double.valueOf(sheet.getRow(startRow - 1).getCell(5).getNumericCellValue()).intValue()));
 					String headerTemplate = "<div class='type2-listen-question-header'>%s</div>";
-					questionBody.setHeader((questionType.trim().equals("NGHE")
+					questionBody.setHeader(questionType.trim().equals("NGHE")
 							? String.format(headerTemplate, sheet.getRow(startRow - 1).getCell(6).toString())
-							: sheet.getRow(startRow - 1).getCell(6).toString())
+							: String.format(headerTemplate, sheet.getRow(startRow - 1).getCell(6).toString())
 									+ generateAudioHtmlBy(sheet.getRow(startRow - 1).getCell(10), hsk,
 											Integer.parseInt(sheet.getSheetName().trim())));
 					if (questionType.equals("NGHE")) {
